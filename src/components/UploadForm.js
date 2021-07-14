9d9ef5e836f2172f12e025df774aeed9159cbc8d
Ray Luna <leon.luna.ray@gmail.com>
2021-07-13 23:10:24 -0700

    upload form state functionality working as intended

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // The upload form may not be needed if uploading directly since there shouldn't be a need for authentication
-
 const UploadForm = () => {
+  // state set to null and will only update if one is selected
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+  // Allowed image types
+  const types = ['image/png', 'image/jpeg'];
+
   const handleChange = (e) => {
-    console.log('changed');
+    // select only the first file if multiples are selected
+    let selectedImg = e.target.files[0];
+    // only set state if a file was selected and is an allowed file type
+    if (selectedImg && types.includes(selectedImg.type)) {
+      setFile(selectedImg);
+      setError('');
+    } else {
+      setFile(null);
+      setError('Please select a valid file type PNG or JPG');
+    }
   };
+
   return (
     <div>
       <form>
         <input type='file' onChange={handleChange} />
+        <div className='output'>
+          {error && <div className='error'>{error}</div>}
+        </div>
       </form>
     </div>
   );
