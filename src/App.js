3343dfce152e852ab26b4b49e0647b44de2325f0
Ray Luna <leon.luna.ray@gmail.com>
2021-07-18 21:36:22 -0700

    modal is fully functional, MVP complete.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import ImageGrid from './components/ImageGrid';
 import Modal from './components/Modal';
 
 function App() {
+  // state for modal display img onClick
   const [selectedImg, setSelectedImg] = useState(null);
 
   return (
     <div className='App'>
       <Title />
+      {/* pass state from ImageGrid to Modal */}
       <ImageGrid setSelectedImg={setSelectedImg} />
-      <Modal selectedImg={selectedImg} />
+      {selectedImg && (
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+      )}
     </div>
   );
 }
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Modal = ({ selectedImg }) => {
+// add functionality to update alt text with user description
+const Modal = ({ selectedImg, setSelectedImg }) => {
+  // close the modal on click of the backdrop, modal will stay open if image is clicked
+  const handleClick = (e) => {
+    if (e.target.classList.contains('backdrop')) {
+      setSelectedImg(null);
+    }
+  };
   return (
-    <div>
-      <img src={selectedImg} alt='lager-img' />
+    <div className='backdrop' onClick={handleClick}>
+      <img src={selectedImg} alt='larger-img' />
     </div>
   );
 };
