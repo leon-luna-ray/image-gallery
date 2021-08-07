2b80b7e6f6dff1559ff4de1fcc7aa3c775ab46b6
Ray Luna <leon.luna.ray@gmail.com>
2021-08-07 02:49:27 -0700

    refactor to manually add images

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React, { useState } from 'react';
 import Title from './components/Title';
-// upload form to be added later as a private route
-import UploadForm from './components/UploadForm';
 import ImageGrid from './components/ImageGrid';
 import Modal from './components/Modal';
-
-// Axios code
-// New process will use axios to get storage data, can then use that hopefully to destructure and get image URLs and other metadata?
-//  possible link https://console.firebase.google.com/u/0/project/image-gallery-1ce58/storage/image-gallery-1ce58.appspot.com/files
-// axios.get('https://PROJECT-NAME.firebaseio.com/users/' + user.uid + '.json?auth=DATABASE-SECRET')
-// .then((response) => {
-//   console.log(response)
-// });
 
 function App() {
   // state for modal display img onClick
@@ -20,8 +10,6 @@ function App() {
   return (
     <div className='App'>
       <Title />
-      <UploadForm />
-      {/* pass state from ImageGrid to Modal */}
       <ImageGrid setSelectedImg={setSelectedImg} />
       {selectedImg && (
         <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
--- src/images/images.js
+++ src/images/images.js
@@ -1,9 +1,25 @@
-// Refactor for direct image posting.
+// Refactor for manual image posting. This will be moved to the database when authentication is complete.
+// create alt prop
 
 const images = [
   {
-    url: '',
-    title: '',
-    alt: '',
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/178427.jpg?alt=media&token=e1433de3-72f8-4979-8579-306bc0fa924a',
+  },
+  {
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/3840x1600-wallpapers-38897-7165184.png?alt=media&token=dfb0a346-c48b-4919-ba81-ce6f00042a00',
+  },
+  {
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/178427.jpg?alt=media&token=e1433de3-72f8-4979-8579-306bc0fa924a',
+  },
+  {
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/3840x1600-wallpapers-38897-7165184.png?alt=media&token=dfb0a346-c48b-4919-ba81-ce6f00042a00',
+  },
+  {
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/178427.jpg?alt=media&token=e1433de3-72f8-4979-8579-306bc0fa924a',
+  },
+  {
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/3840x1600-wallpapers-38897-7165184.png?alt=media&token=dfb0a346-c48b-4919-ba81-ce6f00042a00',
   },
 ];
+
+export default images;
