6532bf9782cb9c923ce960e80122509dc817d99b
Ray Luna <leon.luna.ray@gmail.com>
2021-08-07 14:31:40 -0700

    successfully imported photos. MVP ready to deploy

--- src/images/images.js
+++ src/images/images.js
@@ -3,22 +3,40 @@
 
 const images = [
   {
-    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/178427.jpg?alt=media&token=e1433de3-72f8-4979-8579-306bc0fa924a',
+    id: 'field',
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/field.jpg?alt=media&token=9b3df960-2d45-4840-81c3-e8c06c507bff',
   },
   {
-    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/3840x1600-wallpapers-38897-7165184.png?alt=media&token=dfb0a346-c48b-4919-ba81-ce6f00042a00',
+    id: 'libby',
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/libby.jpg?alt=media&token=69c04dca-64b3-4d7f-8d3c-c5273827260d',
   },
   {
-    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/178427.jpg?alt=media&token=e1433de3-72f8-4979-8579-306bc0fa924a',
+    id: 'rock',
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/rock.jpg?alt=media&token=25f2c6dd-739d-4e39-8637-c3e8a77ec7fb',
   },
   {
-    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/3840x1600-wallpapers-38897-7165184.png?alt=media&token=dfb0a346-c48b-4919-ba81-ce6f00042a00',
+    id: 'alex',
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/alex-grass.jpg?alt=media&token=4c91c0ef-a969-461b-a1bf-d773ace08630',
   },
   {
-    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/178427.jpg?alt=media&token=e1433de3-72f8-4979-8579-306bc0fa924a',
+    id: 'fisherman',
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/fisherman.jpg?alt=media&token=348542ec-0e74-446f-b932-e0e2c4863854',
   },
   {
-    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/3840x1600-wallpapers-38897-7165184.png?alt=media&token=dfb0a346-c48b-4919-ba81-ce6f00042a00',
+    id: 'me',
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/me.jpg?alt=media&token=202002c3-a072-41e9-beda-e701d63dbf4a',
+  },
+  {
+    id: 'alex-nidia',
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/alex-nidia.jpg?alt=media&token=34521480-1df6-4f45-8ffa-ca12703fcf1b',
+  },
+  {
+    id: 'bravo',
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/bravo.jpg?alt=media&token=aa3acb9c-8bec-46f6-84ae-7a1c5d735698',
+  },
+  {
+    id: 'pumpkin',
+    url: 'https://firebasestorage.googleapis.com/v0/b/image-gallery-1ce58.appspot.com/o/pumpkin.jpg?alt=media&token=e85f01f4-bff5-49d8-894c-c1026fa702fe',
   },
 ];
 
