c9cf7fd6b1cd8b8c34cd764b47aa6b2c6d8a4ee8
Ray Luna <leon.luna.ray@gmail.com>
2022-05-24 23:02:37 -0700

    started refactor to work directly with wagtail api. successfully made api call and got content to the page. Working on cors issue for images

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,46 @@
 import React from 'react';
-import { client } from '../client';
+// import { client } from '../client';
+import axios from 'axios';
 import Header from './Header';
 import ImageGrid from './ImageGrid';
 import images from './images';
 import '../styles/App.css';
 
 class App extends React.Component {
-
   state = {
     posts: null,
   };
 
   async componentDidMount() {
     try {
-      const imagePosts = await client.getEntries({
-        content_type: 'imagePost',
-      })
-      console.log({imagePosts})
-      this.setState({
-        posts: imagePosts,
-      });
+      // GET HOME PAGE DATA
+      const userPosts = await axios.get(
+        // TODO: Set the base api url to state or way to easily change the search
+        'http://localhost:8000/api/v2/pages/?type=home.GalleryPost&fields=hero_image,body'
+      );
+      // TODO verify if this will work later on
+      if (userPosts.status === 200) {
+        this.setState({
+          posts: userPosts.data.items,
+        });
+      }
     } catch (error) {
       console.log(error);
     }
   }
 
   render() {
+    if (!this.state.posts) {
+      return (
+        <div>
+          <Header />
+        </div>
+      );
+    }
     return (
       <div>
         <Header />
-        <ImageGrid images={images} />
+        <ImageGrid images={images} posts={this.state.posts} />
       </div>
     );
   }
--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import { Container, Col, Row, Image } from 'react-bootstrap';
 
-const ImageGrid = ({ images }) => {
+const ImageGrid = ({ images, posts }) => {
   // Map images array and return formatted image (map must always return back into the new array it creates)
+  console.log(posts);
+  // console.log(images);
+
+  const renderPosts = posts.map((post, index) => {
+    const { hero_image, title, meta } = post;
+    // TODO change this later for production
+    const imgUrl = `https://localhost:8000${hero_image.meta.download_url}`
+    console.log(post)
+    return (
+      <Col key={index} md={6} xl={4}>
+        <div className='grid-img-div'>
+          <Image
+            className='grid-img'
+            src={hero_image.meta.detail_url}
+            // alt={image.alt}
+            thumbnail
+            fluid
+          />
+        </div>
+
+        <p className='img-caption text-capitalize'>{title}</p>
+      </Col>
+    );
+  });
 
   return (
     <Container className='text-center'>
-      <Row>
-        {images.map((image, index) => {
-          return (
-            <Col key={index} md={6} xl={4}>
-              <div className='grid-img-div'>
-                <Image
-                  className='grid-img'
-                  src={image.url}
-                  alt={image.alt}
-                  thumbnail
-                  fluid
-                />
-              </div>
-
-              <p className='img-caption text-capitalize'>{image.title}</p>
-            </Col>
-          );
-        })}
-      </Row>
+      <Row>{renderPosts}</Row>
     </Container>
   );
 };
